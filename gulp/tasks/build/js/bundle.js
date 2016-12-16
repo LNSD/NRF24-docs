26a802d294f136b7ad167f66deb755e3315f07e4
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2016-12-16 15:51:53 +0100

    Build-js tasks working

---
+++ gulp/tasks/build/js/bundle.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import gutil  from 'gulp-util';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import sourcemaps from 'gulp-sourcemaps';
+
+/* Concat scripts files (minify if --type production) */
+module.exports = function (gulp, config, connect) {
+    return gulp.src(config.build.js.bundle.in)
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        //only uglify if gulp is ran with '--type production'
+        .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.build.js.bundle.out))
+        .pipe(connect.reload());
+};
+
